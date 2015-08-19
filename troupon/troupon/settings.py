2eb6033bba2b810726046e33f21001740a9c75fa
andela-sjames <samuel.james@andela.com>
2015-08-19 13:23:51 +0100

    [#101294918] comments removed

--- troupon/troupon/settings.py
+++ troupon/troupon/settings.py
@@ -85,14 +85,7 @@ DATABASES = {
     }
 }
 
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-'''
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
