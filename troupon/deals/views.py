d7136749ac45458760b816bde2ff329c5e6f1d2f
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-08-27 12:49:18 +0100

    integrate with bootstrap, navbar scaffolding

--- troupon/deals/views.py
+++ troupon/deals/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def landing(request):
+    return render(request, 'base.html')
--- troupon/troupon/urls.py
+++ troupon/troupon/urls.py
@@ -1,7 +1,9 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from deals import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.landing, name='landing')
 ]
