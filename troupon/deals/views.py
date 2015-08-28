1be17caed2eec385aff96972b69343cd66296682
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-08-28 18:37:45 +0100

    create base.html template

--- troupon/deals/views.py
+++ troupon/deals/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def landing(request):
-    return render(request, 'base.html')
+    return render(request, 'base.html', {'show_subscribe': True})
