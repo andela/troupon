cca94828fafb7c70ddbf2511b09243ab0ff57eaf
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-08-29 08:20:45 +0100

    [#102173916] Add test for `/auth` route

--- troupon/deals/tests/test_routes.py
+++ troupon/deals/tests/test_routes.py
@@ -1,5 +1,15 @@
 # -*- coding:utf-8 -*-
-
-from django.test import TestCase
-
 # All your tests for models used by the deals app go here
+
+from django.test import TestCase, Client
+from django.template.loader import render_to_string
+from deals.views import signup
+
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_route_signup(self):
+        response = self.client.get('/auth')
+        self.assertEquals(response.status_code, 200)
