3a0af9f83097d05e69d057ce1d8746572f4ad89f
Samuel James <samuel.james@andela.com>
2015-08-29 09:20:29 +0100

    [#102155530] modified templates, now two templates folder,one root and one app

--- troupon/deals/views.py
+++ troupon/deals/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render,render_to_response
 
 
 # Create your views here.
-def landing(request):
-    return render(request, 'base.html', {'show_subscribe': True})
-
 
 def signup(request):
     return render_to_response('signin_register.html')
--- troupon/troupon/urls.py
+++ troupon/troupon/urls.py
@@ -5,6 +5,6 @@ from deals import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.landing, name='landing'),
-    url(r'^signup/$',views.signup, name = 'signup'),
+    url(r'^', include('deals.urls')),
+    
 ]
