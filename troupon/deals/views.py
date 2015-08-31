271e64b563edc9b23e218a22d87b41f150675b2e
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-08-31 14:03:01 +0100

    [#102150386] create header for landing page

--- troupon/deals/views.py
+++ troupon/deals/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render,render_to_response
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 # Create your views here.
@@ -13,5 +14,7 @@ class LandingPage(TemplateView):
     """class that handles display of landing page/homepage"""
 
     template_name = "deals/landing_page.html"
+    context_var = {'show_subscribe': True}
 
-
+    def get(self, request):
+        return render(request, self.template_name, self.context_var)
