11f8fb106be7f4396173a8a755fd6f9b84b63b47
Uzo Awili <uzo.awili@andela.com>
2015-09-02 20:16:23 +0100

    [#102569504] Feature-Forgot-Password: corrected errors in hash class and hash_tests

--- troupon/account/hashs.py
+++ troupon/account/hashs.py
@@ -10,28 +10,27 @@ class UserHashUtils:
     """ NOTE: This class has the Hashids package as a dependency. 
         Run 'pip install requirements.txt' to install on your environment. """
 
-    timehash_min_length = 15
-    userhash_min_length = 35
-    alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    timehash_min_length = 40
+    userhash_min_length = 20
+    alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     delim = "_"
 
     @staticmethod
-    def gen_hash(user_account):
-        """ accepts a intance of user account and returns a reversible 'time-unique' hash for it """
-
+    def gen_hash(registered_account):
+        """ accepts a intsance of user account and returns a reversible 'time-unique' hash for it """
         # get a timestamp (to make each generated hash unique):
         timestamp = int(time() * 1000)
 
         # encode the timestamp with secret_key:
         hashids = Hashids(salt=secret_key, min_length=UserHashUtils.timehash_min_length, alphabet=UserHashUtils.alphabet)
-        timestamp_hash = hashids.encode(int(time() * 1000))
-
-        # encode the user's email with timestamp:
+        timestamp_hash = hashids.encode(timestamp)
+        
+        # encode the user's pk with timestamp:
         hashids = Hashids(salt=str(timestamp), min_length=UserHashUtils.userhash_min_length, alphabet=UserHashUtils.alphabet)
-        user_email_hash = hashids.encode(user_account.email)
-
+        pk_hash = hashids.encode(registered_account.pk)
+        
         # return the combination delimited by UserHashUtils.delim:
-        return "%s%s%s" % (user_email_hash, UserHashUtils.delim, timestamp_hash)
+        return "%s%s%s" % (timestamp_hash, UserHashUtils.delim, pk_hash)
 
     @staticmethod
     def reverse_hash(hash_str):
@@ -39,25 +38,24 @@ class UserHashUtils:
             Returns None if decoded user does not exits """
 
         # split the hash_str with the delim:
-        hash_list = hash_str.split(UserHashUtils.delim)
-
+        hashs = hash_str.split(UserHashUtils.delim)
+        
         # ensure the list has only 2 parts 
-        if len(hash_list) != 2:
+        if len(hashs) != 2:
             return None
 
-        # decode the timestamp_hash (i.e hash_list[1] ) with the app secret key:
+        # decode the timestamp_hash (i.e hashs[0] ) with the app secret key:
         hashids = Hashids(salt=secret_key, min_length=UserHashUtils.timehash_min_length, alphabet=UserHashUtils.alphabet)
-        timestamp = hashids.decode(hash_list[1])
-
-        # decode the user_email_hash (i.e hash_list[0] ) with the timestamp:
-        hashids = Hashids(salt=str(timestamp), min_length=UserHashUtils.timehash_min_length, alphabet=UserHashUtils.alphabet)
-        user_email = hashids.decode(hash_list[0])
-
+        timestamp = hashids.decode(hashs[0])[0]
+        
+        # decode the pk_hash (i.e hashs[1] ) with the timestamp:
+        hashids = Hashids(salt=str(timestamp), min_length=UserHashUtils.userhash_min_length, alphabet=UserHashUtils.alphabet)
+        account_pk = hashids.decode(hashs[1])[0]
+        
         try:
-            # retrun the account for that email if it exists:
-            registered_account = Account.objects.get(email__exact=user_email)
+            # return the account for that pk if it exists:
+            registered_account = Account.objects.get(pk=account_pk)
             return registered_account
-            
         except ObjectDoesNotExist:
-             # return None if it doesn't:
+            # return None if it doesn't:
             return None
--- troupon/account/tests/test_hashs.py
+++ troupon/account/tests/test_hashs.py
@@ -9,27 +9,32 @@ class AccountHashsTestCase(TestCase):
     def setUp(self):
         # create a test client:
         self.client = Client()
-        # register a smaple user:
+        # register a sample user:
         self.registered_account = Account.objects.create_user('AwiliUzo', 'awillionaire@gmail.com', 'Young1491')
         self.registered_account.first_name = 'Uzo'
         self.registered_account.last_name = 'Awili'
         self.registered_account.save()
 
+
     def test_gen_hash_returns_min_50_chars(self):
         generated_hash = Hasher.gen_hash(self.registered_account)
         self.assertGreaterEqual(len(generated_hash), 50)
+
 
     def test_reverse_hash_returns_user_instance(self):
         generated_hash = Hasher.gen_hash(self.registered_account)
         reversed_hash_result = Hasher.reverse_hash(generated_hash)
         self.assertIsInstance(reversed_hash_result, Account)
 
+
     def test_reverse_hash_returns_None_for_Wrong_hash(self):
         generated_hash = "a2374920910"
         reversed_hash_result = Hasher.reverse_hash(generated_hash)
         self.assertEquals(reversed_hash_result, None)
 
+
     def test_generated_hash_reverses_correctly(self):
         generated_hash = Hasher.gen_hash(self.registered_account)
         reversed_hash_result = Hasher.reverse_hash(generated_hash)
         self.assertEquals(self.registered_account.pk, reversed_hash_result.pk)
+        # self.assertEquals(self.registered_account.pk, 1)
