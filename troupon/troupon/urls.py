29f53e2d57919c7489e29e6428deefea725f58a6
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-02 15:54:24 +0100

    [#102567788] feature-signin: Add base url `/auth` for *account* app

--- troupon/troupon/urls.py
+++ troupon/troupon/urls.py
@@ -2,9 +2,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from deals import views
-from auth import views
+from account import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^auth/', include('account.urls')),
 
 ]
