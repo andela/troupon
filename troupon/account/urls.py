6de53bebc0a542ed2728d780e9661885e6c73320
Samuel James <samuel.james@andela.com>
2015-09-02 16:25:04 +0100

    accounts/urls.py route added

--- troupon/account/urls.py
+++ troupon/account/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import include, url
+
+from django.conf.urls import patterns, include, url
 from account.views import UserSigninView
 
 urlpatterns = [
     url(r'^signin/$', UserSigninView.as_view()),
 ]
+
+
