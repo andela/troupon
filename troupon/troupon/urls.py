70df1c0709fdbcbf73bc7445f2648c584d2d5b2f
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-02 19:23:00 +0100

    [#102567788] feature-signin: Add urls for account app

--- troupon/troupon/urls.py
+++ troupon/troupon/urls.py
@@ -1,11 +1,9 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
-from deals import views
-from account import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^auth/', include('account.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 
 ]
