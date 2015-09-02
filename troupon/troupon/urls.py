b1b21384416f9e3650a7511f15658ef8672e4ddd
Samuel James <samuel.james@andela.com>
2015-09-02 12:13:55 +0100

    added routes to apps in troupon/urls.py

--- troupon/troupon/urls.py
+++ troupon/troupon/urls.py
@@ -2,7 +2,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from deals import views
+from auth import deals
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('deals.urls')),
+    url(r'^', include('auth.urls')),
+
 ]
