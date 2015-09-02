8c28f72b3381673eec1e34dc9d7f3edd29b09f5f
Samuel James <samuel.james@andela.com>
2015-09-02 16:25:04 +0100

    accounts/urls.py route added

--- troupon/account/urls.py
+++ troupon/account/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, url
 from account.views import UserSigninView, ForgotPasswordView, ResetPasswordView
+from account import views
 
 urlpatterns = [
     url(r'^signin/$', UserSigninView.as_view(), name='signin'),
     url(r'^recovery/$', ForgotPasswordView.as_view(), name='account_forgot_password'),
     url(r'^recovery/(?P<recovery_hash>([a-z0-9A-Z])+)$', ResetPasswordView.as_view(), name='account_reset_password'),
 ]
+
+
