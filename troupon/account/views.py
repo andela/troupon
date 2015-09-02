35718c83192aa9f8e40f5676dca37ff93d1a5ef2
Uzo Awili <uzo.awili@andela.com>
2015-09-02 20:50:01 +0100

    [#102569504] Feature-Forgot-Password: created email form class.

--- troupon/account/views.py
+++ troupon/account/views.py
@@ -1,18 +1,17 @@
 from django.views.generic.base import View
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
-# Create your views here.
+from forms import EmailForm
 
 class ForgotPasswordView(View):
 
     def get(self, request, *args, **kwargs):
 
         return HttpResponse('Hi, This is just a placeholder for forgot_password.html')
-        # context = {
-        #     'search_form': 'placeholder',
-        # }
-        # return render(request, 'account/forgot_password.html', context)
+        context = {
+            'email_form': EmailForm(auto_id=True),
+        }
+        return render(request, 'account/forgot_password.html', context)
 
 
     def post(self, request, *args, **kwargs):
