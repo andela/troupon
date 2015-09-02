eef58cdcb4fc9fd5defdc5275e7acb63c56de172
Uzo Awili <uzo.awili@andela.com>
2015-09-02 15:41:30 +0100

    renamed the 'auth' app to 'account' app

--- troupon/auth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-# from views import ForgotPasswordView
-
-urlpatterns = [
-    # url(r'^auth/forgot_password/$', ForgotPasswordView.as_view(), name='Signup'),
-    # url(r'^$', LandingPage.as_view(), name='homepage'),
-]
