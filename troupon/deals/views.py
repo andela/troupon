3effefd3eca458107f2dba54a52b0d32378fdd88
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-09-02 15:55:40 +0100

    [#102567152] create homepage view

--- troupon/deals/views.py
+++ troupon/deals/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 # Create your views here.
+class HomePage(TemplateView):
+    """class that handles display of the homepage"""
+
+    template_name = "deals/homepage.html"
+    context_var = {'show_subscribe': True}
+
+    def get(self, request):
+        return render(request, self.template_name, self.context_var)
