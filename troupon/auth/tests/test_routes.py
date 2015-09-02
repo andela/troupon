27eaade44d80d32f5b78d1f96389fffde80018d1
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-02 14:56:10 +0100

    [#102567788] Create testcase class for `/auth/signin/` route

--- troupon/auth/tests/test_routes.py
+++ troupon/auth/tests/test_routes.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.test import TestCase, Client
-from django.template.loader import render_to_string
+
+
+class AuthentationSignInTestCase(TestCase):
+    """Test that post and get requests to signin routes is successful
+    """
+    
+    def setUp(self):
+        self.client = Client()
+
+    def test_route_get_auth_signin(self):
+        response = self.client.get('/auth/signin')
+        self.assertEquals(response.status_code, 200)
+
+    def test_route_post_auth_signin(self):
+        response = self.client.post('/auth/signin')
+        self.assertEquals(response.status_code, 200)
 
