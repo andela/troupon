eefe2b36be17dc29baf8c3931778ad51164272f7
Samuel James <samuel.james@andela.com>
2015-09-02 23:04:58 +0100

    [#102560626] added test for dummy confirmation page

--- troupon/account/urls.py
+++ troupon/account/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, url
 from account.views import UserSigninView, ForgotPasswordView, ResetPasswordView
 from account import views
-
 urlpatterns = [
     url(r'^signin/$', UserSigninView.as_view(), name='signin'),
     url(r'^recovery/$', ForgotPasswordView.as_view(), name='account_forgot_password'),
