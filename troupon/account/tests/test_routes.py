d687c25d954ee4115b83014ec71488cd145734fe
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-02 19:30:51 +0100

    [#102567788] Add trailing slash to url paths

--- troupon/account/tests/test_routes.py
+++ troupon/account/tests/test_routes.py
@@ -11,10 +11,10 @@ class AuthentationSignInTestCase(TestCase):
         self.client = Client()
 
     def test_route_get_auth_signin(self):
-        response = self.client.get('/auth/signin')
+        response = self.client.get('/auth/signin/')
         self.assertEquals(response.status_code, 200)
 
     def test_route_post_auth_signin(self):
-        response = self.client.post('/auth/signin')
+        response = self.client.post('/auth/signin/')
         self.assertEquals(response.status_code, 200)
 
