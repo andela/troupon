8d58c39663a62b89d4947cc2d934d01407a5aade
Samuel James <samuel.james@andela.com>
2015-09-03 16:11:19 +0100

    [#102560626] using instantiated class of Mysignuform

--- troupon/account/urls.py
+++ troupon/account/urls.py
@@ -2,6 +2,11 @@
 from django.conf.urls import patterns, include, url
 from account.views import UserSigninView
 
-urlpatterns = [
+urlpatterns = patterns('',
+    url(r'^signup/$',views.UserSignupView.as_view(),name = 'UserSignupView'),
+    url(r'^signupreq/$',views.UserSignupreq.as_view(),name = 'UserSignupreq'),
     url(r'^signin/$', UserSigninView.as_view()),
-]
+    url(r'^confirm/$',views.Userconfirm.as_view(),name = 'Userconfirm'),
+
+      
+)
