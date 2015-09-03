6350b646c14d8409bd995a9495b62b506c178875
Samuel James <samuel.james@andela.com>
2015-09-03 15:15:01 +0100

    [#102560626] modified views.py to classbased view

--- troupon/account/urls.py
+++ troupon/account/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from account import views
 
 urlpatterns = patterns('',
-      url(r'^signup/$',views.UserSignin,name = 'UserSignup'),
-      url(r'^signupreq/$',views.UserSignin,name = 'UserSignupreq'),
+      url(r'^signup/$',views.UserSignupView,name = 'UserSignupView'),
+      url(r'^signupreq/$',views.UserSignupreq,name = 'UserSignupreq'),
       url(r'^confirm/$',views.Userconfirm,name = 'Userconfirm'),
 
       
--- troupon/account/views.py
+++ troupon/account/views.py
@@ -1,33 +1,41 @@
 from django.shortcuts import render
 from account.forms import MySignupForm
 from django.views.generic.base import TemplateView
+from django.views.generic import View
+from django.shortcuts import render,render_to_response
+from django.http import HttpResponseRedirect
 from django.core.context_processors import csrf
 
 # Create your views here.
-def UserSignupreq(request):
-    if request.method == 'POST':
-        MySignupForm.username = MySignupForm(request.POST.get('username',''))
-        MySignupForm.email = MySignupForm(request.POST.get('email',''))
-        MySignupForm.first_name = MySignupForm(request.POST.get('first_name',''))
-        MySignupForm.last_name = MySignupForm(request.POST.get('last_name',''))
-        MySignupForm.password = MySignupForm(request.POST.get('password',''))
-        MySignupForm.confirm_password = MySignupForm(request.POST.get('confirm_password',''))
-        if MySignupForm.is_valid():
-            MySignupForm.save()
-            return HttpResponseRedirect('/auth/confirm/')
+class UserSignupreq(View):
+  template_name = 'signup.html'
+  def post(self,request):
+    MySignupForm.username = MySignupForm(request.POST.get('username',''))
+    MySignupForm.email = MySignupForm(request.POST.get('email',''))
+    MySignupForm.first_name = MySignupForm(request.POST.get('first_name',''))
+    MySignupForm.last_name = MySignupForm(request.POST.get('last_name',''))
+    MySignupForm.password = MySignupForm(request.POST.get('password',''))
+    MySignupForm.confirm_password = MySignupForm(request.POST.get('confirm_password',''))
+
+    if MySignupForm.is_valid():
+        MySignupForm.save()
+        return HttpResponseRedirect('/auth/confirm/')
 
     else:
       return HttpResponseRedirect('/auth/signup/')
+ 
 
 
 
-class UserSignup(TemplateView):
-  template_name = signup.html
+class UserSignupView(TemplateView):
+  template_name = 'signup.html'
 
   def get_context_data(self, **kwargs):
-        context = super(UserSignup, self).get_context_data()
+        auth_token = csrf(self.request)
+        context = super(UserSignupView, self).get_context_data(**kwargs)
+        context['csrf_token'] = auth_token
         return context
 
 
-
+#args.update(csrf(request)),
 
