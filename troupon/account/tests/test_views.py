b9fbcd386c97d7b8bf96dccfb67b0e5dd7ff686b
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-03 10:04:13 +0100

    [#102567788] feature-signin: Tests that user requests bind to a view

---
+++ troupon/account/tests/test_views.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase, Client
+from django.core.urlresolvers import resolve
+from django.contrib.auth.models import User
+
+
+class UserSignInViewTestCase(TestCase):
+    """Test that post and get requests to signin routes is successful
+    """
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User(username='johndoe@gmail.com')
+        self.user.set_password('12345')
+        self.user.save()
+
+    def test_view_get_auth_signin(self):
+        """Test that user request for signin page binds to a view called
+            the class name `UserSigninView`.
+        """
+
+        response = resolve('/auth/signin/')
+        self.assertEquals(response.func.__name__, 'UserSigninView')
+
+    def test_view_post_auth_signin(self):
+        """Test that user post to signin route has a session
+        """
+        data = {'email': 'johndoe@gmail.com', 'password': '12345'}
+        response = self.client.post('/auth/signin/', data)
+        self.assertIn('deals', response.content)
