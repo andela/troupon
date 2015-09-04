bb9d6000a0de157328cdd7eb789f4f8d11d285a9
Samuel James <samuel.james@andela.com>
2015-09-04 10:38:47 +0100

    [#102560626] changed user object to User.. Major change to forms.py removed Account Model Model now User in forms.py Meta Class

--- troupon/account/forms.py
+++ troupon/account/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
-from account.models import Account
-from django.forms import ModelForm 
-from django import forms
+from django.utils import timezone
+from django import forms 
 
 
 class MySignupForm(UserCreationForm):
@@ -13,20 +12,20 @@ class MySignupForm(UserCreationForm):
 
 
 
-    class meta:
-        model = Account
-        fields = ('id','email', 'username','password1','password2','first_name', 'last_name')
+    class meta:        
+        fields = ('email', 'username','password1','password2','first_name', 'last_name')
 
         
         def save(self, commit=True):
             user = super(MySignupForm, self).save(commit=False)
+            user.set_password(self.cleaned_data["password1"])
             user.email = self.cleaned_data['email']
             user.first_name = self.cleaned_data['first_name']
             user.last_name = self.cleaned_data['last_name']
-        
+            user.is_active = False
+            
 
-        if commit:
-            user.save()
-        return user
-
+            if commit:
+                user.save()
+            return user
 
--- troupon/account/models.py
+++ troupon/account/models.py
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 
 from django.db import models
 
 
+=======
+from django.db import models
+# Create your models here.
+>>>>>>> [#102560626] changed user object to User.. Major change to forms.py removed Account Model Model now User in forms.py Meta Class
 
 # Create your models here.
