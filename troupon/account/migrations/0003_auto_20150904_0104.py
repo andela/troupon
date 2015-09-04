719f9ae5ffff6148f47de50dcc696068f2c4ef01
Samuel James <samuel.james@andela.com>
2015-09-04 10:38:47 +0100

    [#102560626] changed user object to User.. Major change to forms.py removed Account Model Model now User in forms.py Meta Class

---
+++ troupon/account/migrations/0003_auto_20150904_0104.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+import django.contrib.auth.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('account', '0002_auto_20150903_2256'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='account',
+            options={'verbose_name': 'user', 'verbose_name_plural': 'users'},
+        ),
+        migrations.AlterModelManagers(
+            name='account',
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
+        ),
+        migrations.RemoveField(
+            model_name='account',
+            name='email',
+        ),
+        migrations.RemoveField(
+            model_name='account',
+            name='first_name',
+        ),
+        migrations.RemoveField(
+            model_name='account',
+            name='id',
+        ),
+        migrations.RemoveField(
+            model_name='account',
+            name='is_active',
+        ),
+        migrations.RemoveField(
+            model_name='account',
+            name='is_admin',
+        ),
+        migrations.RemoveField(
+            model_name='account',
+            name='last_login',
+        ),
+        migrations.RemoveField(
+            model_name='account',
+            name='last_name',
+        ),
+        migrations.RemoveField(
+            model_name='account',
+            name='password',
+        ),
+        migrations.RemoveField(
+            model_name='account',
+            name='username',
+        ),
+        migrations.AddField(
+            model_name='account',
+            name='user_ptr',
+            field=models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, default=django.utils.timezone.now, serialize=False, to=settings.AUTH_USER_MODEL),
+            preserve_default=False,
+        ),
+    ]
--- troupon/account/models.py
+++ troupon/account/models.py
@@ -1,24 +1,3 @@
-from django.contrib.auth.models import BaseUserManager
-from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
 from django.db import models
-
 # Create your models here.
-class Account(AbstractUser):
-    email = models.EmailField(max_length=254,unique=True)
-    username = models.CharField(max_length=40, unique=True)
-
-    first_name = models.CharField(max_length=40, blank=True)
-    last_name = models.CharField(max_length=40, blank=True)
-
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    def __unicode__(self):
-        return self.email
-
 
