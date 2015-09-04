efe12d56c8e4d792f4267f030b815615835cdba5
Samuel James <samuel.james@andela.com>
2015-09-04 12:48:59 +0100

    [#102560626] using AbstractUser object

--- troupon/account/forms.py
+++ troupon/account/forms.py
@@ -1,31 +1,27 @@
+from django import forms
+from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.forms import UserCreationForm
-from django.utils import timezone
-from django import forms 
-
-
+ 
 class MySignupForm(UserCreationForm):
     email = forms.EmailField(required = True)
-    username = forms.CharField(required = True)
     first_name = forms.CharField(required = True)
-    last_name = forms.CharField(required = True)
+    last_name = forms.CharField(required=True)
+
+
+    class meta:
+        model = AbstractUser
+        fields = ('first_name','last_name','username','email','password1','password2')
 
 
 
+    def save(self, commit=True):
+        user = super(MySignupForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.is_staff = True 
 
-    class meta:        
-        fields = ('email', 'username','password1','password2','first_name', 'last_name')
-
-        
-        def save(self, commit=True):
-            user = super(MySignupForm, self).save(commit=False)
-            user.set_password(self.cleaned_data["password1"])
-            user.email = self.cleaned_data['email']
-            user.first_name = self.cleaned_data['first_name']
-            user.last_name = self.cleaned_data['last_name']
-            user.is_active = False
-            
-
-            if commit:
-                user.save()
-            return user
+        if commit:
+            user.save()
+        return user
 
