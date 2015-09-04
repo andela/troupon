45891fc00495a1bf460b3b538ddd002320d549e5
Samuel James <samuel.james@andela.com>
2015-09-04 17:25:39 +0100

    [#102560626]final rebase

--- troupon/account/models.py
+++ troupon/account/models.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 
 from django.db import models
-
-
-=======
-from django.db import models
-# Create your models here.
->>>>>>> [#102560626] changed user object to User.. Major change to forms.py removed Account Model Model now User in forms.py Meta Class
-
 # Create your models here.
