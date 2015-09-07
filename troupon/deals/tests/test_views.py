f81f13ef368cbf8a250dce1b3ecfe1ffcbc28b7a
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-09-07 17:14:02 +0100

    [#102755798] test if social links are present on homepage

--- troupon/deals/tests/test_views.py
+++ troupon/deals/tests/test_views.py
@@ -36,6 +36,15 @@ class HomepageViewTests(unittest.TestCase):
         driver.get("http://127.0.0.1:8000/")
         assert "About Troupon" in driver.page_source
 
+    def test_social_links(self,):
+        """
+        Checks social links are working
+        """
+        driver = self.driver
+        driver.get("http://127.0.0.1:8000/")
+        assert "http://facebook.com/troupon/" in driver.page_source
+        assert "http://twitter.com/troupon/" in driver.page_source
+
     def tearDown(self,):
         """
         Close the browser window
