59bc2c38821a0fc20b706aae941e3d2e5b0263d2
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-10 23:14:47 +0100

    [#102761572] Test that GET request for a deal is successful

--- troupon/deals/tests/test_routes.py
+++ troupon/deals/tests/test_routes.py
@@ -20,3 +20,14 @@ class HomepageRouteTests(TestCase):
 
         response = self.client.get(reverse('homepage'))
         self.assertEqual(response.status_code, 200)
+
+
+class SingleDealViewTestCase(TestCase):
+    """
+        This contains tests to check that an HTTP GET
+        request for a deal is successful
+    """
+
+    def test_single_deal_view(self):
+        response = self.client.get('/deals/1')
+        self.assertEqual(response.status_code, 200)
