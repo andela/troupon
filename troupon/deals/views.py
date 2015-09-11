63e0f6fd48cc379ca58eaceb572146c802ef4963
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-11 07:10:08 +0100

    [#102761572] feature-single-deal-view: respond to request for deals - filter by id

--- troupon/deals/views.py
+++ troupon/deals/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.http import HttpResponse
+from deals.models import Deal
+from django.template import Template, Context
 
 
 # Create your views here.
@@ -11,3 +14,19 @@ class HomePage(TemplateView):
 
     def get(self, request):
         return render(request, self.template_name, self.context_var)
+
+
+class SingleDealView(View):
+    cls_default_msg = {
+                        "result": "We're still working on this deal for you.",
+                      }  # Default message display when deal's not found
+
+    def get(self, *args, **kwargs):
+        deal_id = self.kwargs.get('deal_id')
+        try:
+            deal = Deal.objects.get(id=deal_id)
+            result = {"result": deal}
+        except Deal.DoesNotExist:
+            result = SingleDealView.cls_default_msg
+        template_stub = Template("{{result}}")
+        return HttpResponse(template_stub.render(Context(result)))
