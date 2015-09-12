ad28d236bb354978636b89b756e2e9891f7547cd
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-09-12 09:08:12 +0100

    [#102839668] Feature: create end to end tests for user signin and registration pages

--- troupon/account/tests/test_views.py
+++ troupon/account/tests/test_views.py
@@ -68,13 +68,53 @@ class ForgotPasswordViewTestCase(TestCase):
 
 # selenium tests for registration, signup and signin templates
 class UserRegisterTestCase(LiveServerTestCase):
+    ''' 
+    End to End testing of user registration and signin pages
+    '''
+
     def setUp(self,):
-        User.objects.create_superuser('admin','admin@example.com','admin')
+        """
+        Setup the test driver
+        """
+        User.objects.create_superuser(
+            'admin','admin@example.com','admin')
         self.driver = webdriver.Firefox()
         super(UserRegisterTestCase, self).setUp()
 
-    
+    def test_signin_user(self,):
+        """
+        Checks if a user can sign in
+        """
+        self.driver.get(
+            "%s%s" %(self.live_server_url, '/signin/'))
+
+        # input login details and submit
+        username = self.driver.find_element_by_id("username").send_keys('admin')
+        password = self.driver.find_element_by_id("password").send_keys('admin')
+        self.driver.find_element_by_xpath("//input[@value='Sign in']").click()
+        
+        # assert that user is logged in by accessing admin area
+        self.driver.get('%s%s' % (self.live_server_url, "/admin/auth/user/add/"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_can_register(self,):
+        """
+        Checks if user can signup on signin page
+        """
+        self.driver.get(
+            "%s%s" %(self.live_server_url, '/signin/'))
+
+        self.driver.find_element_by_id("user_signup_link").click()
+        self.driver.find_element_by_id("create_username").send_keys("tosin")
+        self.driver.find_element_by_id("create_password1").send_keys("tosin")
+        self.driver.find_element_by_id("create_password2").send_keys("tosin")
+        self.driver.find_element_by_id("create_email").send_keys("tosin@andela.com")
+        self.driver.find_element_by_id("create_user_form").submit()
+        self.assertIn("Success! your account has been created", self.driver.page_source)
 
     def tearDown(self,):
+        """
+        Close the browser window
+        """
         self.driver.close()
         super(UserRegisterTestCase, self).tearDown()
