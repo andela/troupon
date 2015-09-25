1f5d4324268a1b5f76b46f6a50c42722d7624273
Uzo Awili <uzo.awili@andela.com>
2015-09-25 08:18:19 +0100

    [102755798] feature-homepage-templates: Refactored layout for base.html

--- troupon/troupon/settings/development.py
+++ troupon/troupon/settings/development.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'troupon',
-        'USER': 'chidieberennadi',
-        'PASSWORD': 'cidman',
+        'USER': 'Administrator',
+        'PASSWORD': 'administrator',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
