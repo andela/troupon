829f47aec531e51a1a3e1c42bc562f35e8cf5974
Uzo Awili <uzo.awili@andela.com>
2015-09-25 08:18:19 +0100

    [102755798] feature-homepage-templates: Refactored layout for base.html

--- troupon/troupon/settings/test.py
+++
@@ -1,9 +0,0 @@
-# Test specific settings
-from .development import *
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(__file__), 'test.db')
-    }
-}
