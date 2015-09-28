4a8fe78891b21507c61e26aac429948e93a8c8f3
Samuel James <samuel.james@andela.com>
2015-09-28 01:43:37 +0100

    [#102779268]modified deal urls.py again

--- troupon/deals/urls.py
+++ troupon/deals/urls.py
@@ -4,6 +4,6 @@ from .views import SingleDealView, DealSearchView
 
 urlpatterns = [
     url(r'^search/', include('haystack.urls')),
-    url(r'^search/entry/$', DealSearchView.as_view(), name = 'dealsearch')
-    url(r'^(?P<deal_id>\d+)/$', SingleDealView.as_view(), name='single_deal'),
+    url(r'^search/entry/$', DealSearchView.as_view(), name = 'dealsearch'),
+    url(r'^(?P<deal_id>\d+)/$', SingleDealView.as_view(), name='singledeal'),
 ]
