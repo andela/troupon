7eddc3b79027cccd8f67574e3171526a14e9195b
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-09-28 17:29:54 +0100

    [#104000366] (feature) Add url mapping to DealsByCategoryView

--- troupon/deals/urls.py
+++ troupon/deals/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import SingleDealView
+from .views import SingleDealView, DealsByCategoryView
 
 
 urlpatterns = [
     url(r'^(?P<deal_id>\d+)/$', SingleDealView.as_view(), name='single_deal'),
+    url(r'^(?P<category_id>\d+)/(?P<category_name>\w+)/$',
+        DealsByCategoryView.as_view()),
 ]
