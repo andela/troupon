8c7bee964c34125589d0de02784a1b73c901140d
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-02 16:22:33 +0100

    (fix) using only DealView to handle deal request

--- troupon/deals/urls.py
+++ troupon/deals/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from .views import SingleDealView
+from .views import DealView
 
 
 urlpatterns = [
-    url(r'^(?P<deal_id>\d+)/$', SingleDealView.as_view(), name='single_deal'),
     url(r'^$', DealView.as_view(), name='deal'),
 ]
