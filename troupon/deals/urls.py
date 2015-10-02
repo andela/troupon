a619ecac04b7cbca8e084abe16d40e174fa4eb51
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-02 06:06:21 +0100

    [#102761572] add url mapping to view for creating deals

--- troupon/deals/urls.py
+++ troupon/deals/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import SingleDealView
+from .views import SingleDealView, DealView
 
 
 urlpatterns = [
-    url(r'^(?P<deal_id>\d+)/$', SingleDealView.as_view(), name='single_deal')
+    url(r'^(?P<deal_id>\d+)/$', SingleDealView.as_view(), name='single_deal'),
+    url(r'^$', DealView.as_view(), name='deal'),
 ]
