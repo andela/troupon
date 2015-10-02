19b18edb6d28844a123e81675877fd303124e351
Uzo Awili <uzo.awili@andela.com>
2015-10-02 09:15:16 +0100

    [102755798] feature-homepage-templates: removes sass

--- troupon/troupon/settings/development.py
+++ troupon/troupon/settings/development.py
@@ -14,6 +14,7 @@ import os
 from django_envie.workroom import convertfiletovars
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+convertfiletovars()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
