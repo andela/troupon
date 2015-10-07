8c8a7503347af2330e5cb77a69415d2c4e3ebd08
Samuel James <samuel.james@andela.com>
2015-10-07 18:23:10 +0100

    [#102779268] made staticfile visible.

---
+++ troupon/staticfiles/js/ajax.1f5bae08ef6c.js
@@ -0,0 +1,28 @@
+$(function(){
+
+    $('#search').keyup(function() {
+
+        $.ajax({
+            type: 'POST',
+            url: 'deals/search/entry',
+            data: { 
+                'q' : $('#search').val(),
+                'csrfmiddlewaretoken' : $('input[name=csrfmiddlewaretoken]').val()
+            },
+            success: searchSuccess,
+            dataType: 'html'
+        });
+
+    });
+
+});
+
+function searchSuccess(data, textStatus, jqXHR)
+{
+    $('#search-results').html(data);
+
+    $('#search-results').mouseleave(function(){
+        $(this).fadeOut('slow');
+        });
+}
+
