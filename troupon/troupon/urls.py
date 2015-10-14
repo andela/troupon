1b2fe55bb95250ce09bc8af4fa379a1f5eaff281
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-14 05:41:52 +0100

    [#105570520] (fix) set url base pattern for deals app in project urls

--- troupon/troupon/urls.py
+++ troupon/troupon/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 import deals
 import account
 
+
 urlpatterns = [
     url(r'^accounts/', include('allaccess.urls')),
     url(r'^account/', include('account.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('deals.urls')),
+    url(r'^deals/', include('deals.urls')),
     url(r'^$', deals.views.HomePage.as_view(), name='homepage'),
 ]
