e18a681ca18557644ebd6d925a0ac43c0270c230
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-15 17:25:01 +0100

    [#102761572] (fix) remove unused import; prepare to mock facebook signup; format to PEP-8 guidelines

--- troupon/account/tests/test_routes.py
+++ troupon/account/tests/test_routes.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.template.loader import render_to_string
 from django.test import TestCase, Client
-from django.core.context_processors import csrf
 from django.core.urlresolvers import resolve
 from django.contrib.auth.models import User
 from account.views import ForgotPasswordView, ResetPasswordView
-from allaccess.views import OAuthRedirect,OAuthCallback
+from allaccess.views import OAuthRedirect, OAuthCallback
+import mock
 
 
 class UserSigninTestCase(TestCase):
@@ -39,12 +38,18 @@ class ForgotPasswordRouteTestCase(TestCase):
         self.assertEquals(response.status_code, 200)
 
     def test_post_forgot_route_returns_200(self):
-        response = self.client.post('/account/recovery/', {"email": "random@mail.com"})
+        response = self.client.post(
+            '/account/recovery/',
+            {"email": "random@mail.com"}
+        )
         self.assertEquals(response.status_code, 200)
 
     def test_forgot_route_resolves_to_correct_view(self):
         response = self.client.get('/account/recovery/')
-        self.assertEqual(response.resolver_match.func.__name__, ForgotPasswordView.as_view().__name__)
+        self.assertEqual(
+            response.resolver_match.func.__name__,
+            ForgotPasswordView.as_view().__name__
+        )
 
 
 class ResetPasswordRouteTestCase(TestCase):
@@ -53,47 +58,53 @@ class ResetPasswordRouteTestCase(TestCase):
         self.client = Client()
 
     def test_reset_route_resolves_to_correct_view(self):
-        response = self.client.get('/account/recovery/ajkzfYba9847DgJ7wbkwAaSbkTjUdawGG998qo3HG8qae83')
-        self.assertEqual(response.resolver_match.func.__name__, ResetPasswordView.as_view().__name__)
+        response = self.client.get(
+            '/account/recovery/ajkzfYba9847DgJ7wbkwAaSbkTjUdawGG998qo3HG8qae83'
+        )
+        self.assertEqual(
+            response.resolver_match.func.__name__,
+            ResetPasswordView.as_view().__name__
+        )
 
 
 class UserRegistrationViewTest(TestCase):
-  '''
-  Test class to user registration.
-  '''
-  def setUp(self):
     '''
-    User sign's up with data.
+    Test class to user registration.
     '''
-    self.client_stub = Client()
-    self.form_data = dict(username="andela",
-                           password1="andela",
-                           password2="andela",
-                           email="andela@andela.com",
-                           )
+    def setUp(self):
+        '''
+        User sign's up with data.
+        '''
+        self.client_stub = Client()
+        self.form_data = dict(
+            username="andela",
+            password1="andela",
+            password2="andela",
+            email="andela@andela.com",
+        )
 
-  def test_view_signup_route(self):
-    '''
-    User signup page is called.
-    '''
+    def test_view_signup_route(self):
+        '''
+        User signup page is called.
+        '''
 
-    response = self.client_stub.get('/account/signup/')
-    self.assertEquals(response.status_code, 200)
+        response = self.client_stub.get('/account/signup/')
+        self.assertEquals(response.status_code, 200)
 
-  def test_view_reg_route(self):
-    '''
-    User is redirected after signup data is validated.
-    '''
-    response = self.client_stub.post('/account/signup/', self.form_data)
-    self.assertEquals(response.status_code, 302)
+    def test_view_reg_route(self):
+        '''
+        User is redirected after signup data is validated.
+        '''
+        response = self.client_stub.post('/account/signup/', self.form_data)
+        self.assertEquals(response.status_code, 302)
 
-  def test_view_reg_success_route(self):
-    '''
-    User gets to view confirmation page after signup.
-    '''
+    def test_view_reg_success_route(self):
+        '''
+        User gets to view confirmation page after signup.
+        '''
 
-    response = self.client_stub.get('/account/confirm/')
-    self.assertEquals(response.status_code, 200)
+        response = self.client_stub.get('/account/confirm/')
+        self.assertEquals(response.status_code, 200)
 
 
 class UserSignInViewTestCase(TestCase):
@@ -121,20 +132,22 @@ class UserSignInViewTestCase(TestCase):
         response = self.client.post('/account/signin/', data)
         self.assertIn('deals', response.content)
 
+
 class FacebookSignupTestCase(TestCase):
 
     def setUp(self):
         self.client_stub = Client()
 
-
     def test_user_signup_via_facebook(self):
         response = self.client_stub.post('/accounts/login/facebook/')
-        self.assertEqual(response.resolver_match.func.__name__, OAuthRedirect.as_view().__name__)
+        self.assertEqual(
+            response.resolver_match.func.__name__,
+            OAuthRedirect.as_view().__name__
+        )
 
-       
     def test_user_redirected_after_facebook_signup(self):
         response = self.client_stub.post('/accounts/callback/facebook/')
-        self.assertEqual(response.resolver_match.func.__name__, OAuthCallback.as_view().__name__)
-
-
-
+        self.assertEqual(
+            response.resolver_match.func.__name__,
+            OAuthCallback.as_view().__name__
+        )
