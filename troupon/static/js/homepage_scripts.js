ca4ba5a57b1cedb56f2617f0000e2d5901377414
Uzo Awili <uzo.awili@andela.com>
2015-10-19 03:16:12 +0100

    [#105569758] feature-homepage-listings: created deal listings section.

--- troupon/static/js/homepage_scripts.js
+++ troupon/static/js/homepage_scripts.js
@@ -2,19 +2,17 @@
 $(document).ready(function() {
 
     //----------------------------------------
-    //  Featured deals gallery:
+    //  Flickity galleries:
     //----------------------------------------
 
-
-    //initialize flickity on the featured deals gallery:
-
-    var $gallery = $('#banner-section .gallery').flickity({
-        
+    //initialize Flickity on the galleries:
+    var $gallery = $('.flickity-gallery').flickity({
+    
+        cellSelector: '.gallery-cell',
         setGallerySize: false,
         freeScroll: true,
         wrapAround: true,
         autoPlay: true,
-        lazyLoad: 1,
         imagesLoaded: true,
 
         arrowShape: { 
@@ -23,39 +21,21 @@ $(document).ready(function() {
             x2: 60, y2: 40,
             x3: 20
         }
+
     });
 
-    /*
-    //set the background color of a loaded gallery cell to the image's dominant color:
-    $gallery.on( 'lazyLoad', function( event, cellElement ){
-        try {
+    //----------------------------------------
+    //  Packery grids:
+    //----------------------------------------
+    
+    //initialize Packery on the grids:
+    var $grid = $('.packery-grid').packery({
 
-            // get the loaded image:
-            var loadedImage = event.originalEvent.target;
-            loadedImage.crossOrigin = "Anonymous";
+        "itemSelector": '.grid-item',
+        "columnWidth": ".grid-sizer",
+        "gutter": ".gutter-sizer",
+        "percentPosition": true
 
-            //get it's domninant color:
-            var colorThief = new ColorThief();
-            var dominantColor = colorThief.getColor(loadedImage);
-
-            //modifiy the cell's background color, sizing and positioning:
-            $( cellElement ).css({
-                "background-color": "rgba(" + dominantColor[0] + "," + dominantColor[1] + "," + dominantColor[2] + ", 1)"
-            });
-
-            //modifiy the cell image's sizing and positioning:
-            $( loadedImage ).css({
-                "width": "100%",
-                "height": "auto",
-                "left": "0px",
-                "top": "50%",
-                "transform": "translateY(-50%)"
-            });
-
-        } catch (e) {
-            console.log(e);
-        }
-    })
-    */
+    });
 
 });
