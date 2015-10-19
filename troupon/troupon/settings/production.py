0b8babb9187bc092662f0f442db2661851b7ca0f
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-19 13:20:33 +0100

    [#105570056] (fix) address all issues with bower; re-order buildpacks; add preinstall command; add packages in bower.json

--- troupon/troupon/settings/production.py
+++ troupon/troupon/settings/production.py
@@ -4,13 +4,22 @@ Production specific settings for troupon project.
 
 from .base import *
 import dj_database_url
+import os
 
+APPLICATION_DIR = os.path.dirname(globals()['__file__'])
 
-DEBUG = False
+DEBUG = True
 
 DATABASES = {
     'default': dj_database_url.config()
 }
+
+BOWER_COMPONENTS_ROOT = os.path.join(
+    APPLICATION_DIR, '..', '..', 'static')
+
+BOWER_PATH = '/app/node_modules/bower'
+
+
 
 # Enable Connection Pooling
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
