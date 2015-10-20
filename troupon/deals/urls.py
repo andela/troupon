b470596939cd9b91bea6344765363d5b240a5ab3
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-20 20:52:56 +0100

    (feature) [#102761572] add routes to DealWithSlugView and DealCategoryView

--- troupon/deals/urls.py
+++ troupon/deals/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url
-from .views import DealView, DealsView
+from .views import DealView, DealWithSlugView, DealCategoryView, DealsView
 
 
 urlpatterns = [
     url(r'^$', DealsView.as_view(), name='deals'),
+    # /deals/87
     url(r'^(?P<deal_id>[0-9]+)$', DealView.as_view(), name='deal'),
+    # /deals/87/spa-treatment-discount
+    url(r'^(?P<deal_id>[0-9]+)/(?P<deal_slug>[\w-]+)/$',
+        DealWithSlugView.as_view(),
+        name='deal-with-slug'),
+    url(r'^listings/$',
+        DealCategoryView.as_view(),
+        name='deal-category-with-slug'),
 ]
