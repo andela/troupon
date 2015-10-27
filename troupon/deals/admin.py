5310e01a9ddda7fac48f091aa396f4148cfb0d01
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-27 09:09:25 +0100

    [105570520] (feature) register deals and category models in admin backend

--- troupon/deals/admin.py
+++ troupon/deals/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from .models import Deal, Advertiser, Category
 
 
-admin.site.register(Deal)
-admin.site.register(Advertiser)
-admin.site.register(Category)
-
-
 class DealAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Deal, DealAdmin)
+admin.site.register(Advertiser)
+admin.site.register(Category, CategoryAdmin)
