340ea987b2515f9bc1fc6e58a31d8fac0430914b
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-30 18:05:02 +0100

    [fixes #102761572]switch to div for laying out map

--- troupon/static/js/map.js
+++ troupon/static/js/map.js
@@ -1,3 +1,4 @@
+<script type="text/javascript">
     $('document').ready(function(){
         MapMaker = {
             geocoder: '',
@@ -37,3 +38,4 @@
 
         MapMaker.initialize();
     });
+</script>
