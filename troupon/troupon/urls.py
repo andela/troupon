78056425131472f62bf1f0feff02a64f53a1a60a
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-11-18 09:20:18 +0100

    [Fix #91] Revert homepage url back to initial line in url.py, more content for support page

--- troupon/troupon/urls.py
+++ troupon/troupon/urls.py
@@ -6,11 +6,11 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', deals.views.HomePageView.as_view(), name='homepage'),
     url(r'^accounts/', include('allaccess.urls')),
     url(r'^account/', include('account.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^deals/', include('deals.urls')),
+    url(r'^$', deals.views.HomePageView.as_view(), name='homepage'),
     url(r'^about/', TemplateView.as_view(template_name="deals/about.html"), name='about'),
     url(r'^investors/', TemplateView.as_view(template_name="deals/investor.html"), name='investor'),
     url(r'^team/', TemplateView.as_view(template_name="deals/team.html"), name='team'),
