e3d756bf9d0835a95bca42ae05650991586776fa
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-11-18 08:35:28 +0100

    [Fix #108322010] Remove commented out code, use TemplateView for static pages

--- troupon/troupon/urls.py
+++ troupon/troupon/urls.py
@@ -2,17 +2,17 @@ from django.conf.urls import include, url
 from django.contrib import admin
 import deals
 import account
-from deals.views import AboutView, InvestorView, TeamView, SupportView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    url(r'^$', deals.views.HomePageView.as_view(), name='homepage'),
     url(r'^accounts/', include('allaccess.urls')),
     url(r'^account/', include('account.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^deals/', include('deals.urls')),
-    url(r'^$', deals.views.HomePageView.as_view(), name='homepage'),
-    url(r'^about/', AboutView.as_view(), name='about'),
-    url(r'^investors/', InvestorView.as_view(), name='investor'),
-    url(r'^team/', TeamView.as_view(), name='team'),
-    url(r'^support/', SupportView.as_view(), name='support'),
+    url(r'^about/', TemplateView.as_view(template_name="deals/about.html"), name='about'),
+    url(r'^investors/', TemplateView.as_view(template_name="deals/investor.html"), name='investor'),
+    url(r'^team/', TemplateView.as_view(template_name="deals/team.html"), name='team'),
+    url(r'^support/', TemplateView.as_view(template_name="deals/support.html"), name='support'),
 ]
