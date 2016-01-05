42e4f4af534750c1a45cbf214a9b208256eef51f
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2016-01-05 11:13:16 +0100

    [Feature #108845484] modify login, logout and login_redirect_url

--- troupon/troupon/settings/base.py
+++ troupon/troupon/settings/base.py
@@ -131,8 +131,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-LOGIN_REDIRECT_URL = '/confirm/'
-LOGIN_URL = '/register/'
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
+LOGOUT_URL = '/'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
