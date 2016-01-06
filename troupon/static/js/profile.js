afc4564816111edaf04fd9a6184c0050b48e289c
Samuel James <samuel.james@andela.com>
2016-01-06 03:40:43 +0100

    [Features #108835916] make js quotes consistent

--- troupon/static/js/profile.js
+++ troupon/static/js/profile.js
@@ -4,49 +4,49 @@ $(document).ready(function() {
     var button  = $('#save');
     button.attr('disabled', 'disabled');
 
-    var telInput = $("#mobile-number")
+    var telInput = $('#mobile-number')
     telInput.intlTelInput({
-        initialCountry: "auto",
+        initialCountry: 'auto',
         geoIpLookup: function(callback) {
-            $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
-            var countryCode = (resp && resp.country) ? resp.country : "";
+            $.get('http://ipinfo.io', function() {}, 'jsonp').always(function(resp) {
+            var countryCode = (resp && resp.country) ? resp.country : '';
             callback(countryCode);
         });
     },
-        utilsScript: "../../static/lib/libphonenumber/build/utils.js",
-        nationalMode: "true"
+        utilsScript: '../../static/lib/libphonenumber/build/utils.js',
+        nationalMode: 'true'
     });
 
-    var errorMsg = $("#error-msg");
-    var validMsg = $("#valid-msg");
+    var errorMsg = $('#error-msg');
+    var validMsg = $('#valid-msg');
 
     var reset = function() {
-        telInput.removeClass("error");
-        errorMsg.addClass("hide");
-        validMsg.addClass("hide");
+        telInput.removeClass('error');
+        errorMsg.addClass('hide');
+        validMsg.addClass('hide');
     };
 
     // on blur: validate
     telInput.blur(function() {
         reset();
             if ($.trim(telInput.val())) {
-            if (telInput.intlTelInput("isValidNumber")) {
-              validMsg.removeClass("hide");
+            if (telInput.intlTelInput('isValidNumber')) {
+              validMsg.removeClass('hide');
             } else {
-              telInput.addClass("error");
-              errorMsg.removeClass("hide");
+              telInput.addClass('error');
+              errorMsg.removeClass('hide');
             }
         }
     });
 
     // on keyup / change flag: reset
-    telInput.on("keyup change", reset);
+    telInput.on('keyup change', reset);
 
-        $("form").submit(function() {
-        $("#hidden").val(telInput.intlTelInput("getNumber"));
+        $('form').submit(function() {
+        $('#hidden').val(telInput.intlTelInput('getNumber'));
     });
         // enable save button on changes in form data
-        $("form :input").change(function() {
+        $('form :input').change(function() {
             $(this).closest('form').data('changed', true);
             button.removeAttr('disabled');
     });
