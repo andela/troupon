4e6c47875055d9990096581af3bc17fcd6a0f4e7
Oluwafemi Sule <oluwafemi.sule@andela.com>
2016-01-06 10:26:25 +0100

    [Feature #108848406] update merchant app urls

--- troupon/merchant/urls.py
+++ troupon/merchant/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from conversations.views import MessagesView, MessageView, ComposeMessageView
+from merchant.views import ManageDealsView, ManageDealView, TransactionsView,\
+    TransactionView, CreateDealView, MerchantView
 
 urlpatterns = [
     # pattern maps to view handling `POST` and `GET`
@@ -26,4 +28,45 @@ urlpatterns = [
         name='message'
     ),
 
+    # pattern maps to view handling `GET request to
+    #  `/merchant/deals`
+    url(
+        r'^deals',
+        ManageDealsView.as_view(), name='merchant_manage_deals',
+    ),
+
+    # pattern maps to view handling `GET` and `POST`
+    # requests to `/merchant/deals/<slug>`
+    url(
+        r'^deals/(?P<slug>[\w-]+)/$',
+        ManageDealView.as_view(), name='merchant_manage_deal',
+    ),
+
+    # pattern maps to view handling `GET` request to
+    #  `/merchant/transactions`
+    url(
+        r'^transactions/$',
+        TransactionsView.as_view(), name='merchant_transactions',
+    ),
+
+    # pattern maps to view handling `GET` and `POST` to
+    #  `/merchant/transactions/<transaction_id>`
+    url(
+        r'^transactions/(?P<transaction_id>[\d]+)/$',
+        TransactionView.as_view(), name='merchant_transaction',
+    ),
+
+    # pattern maps to view handling `GET` and `POST` to
+    #  `/merchant/deals/create`
+    url(
+        r'^deals/create/$',
+        CreateDealView.as_view(), name='merchant_create_deal',
+    ),
+
+    # pattern maps to view handling `GET` to
+    #  `/merchant/<merchant_slug>`
+    url(
+        r'^merchant/(?P<merchant_slug>[\w-]+)/$',
+        MerchantView.as_view(), name='merchant_view',
+    ),
 ]
