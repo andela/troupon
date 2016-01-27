5b1a5f6b857a3908fc21ea7f5cfd69ed2a040612
Amos Omondi <amos.omondi@andela.com>
2016-01-27 14:21:30 +0300

    [Chore #111761552] Document forms.py in account app.

--- troupon/account/forms.py
+++ troupon/account/forms.py
@@ -1,10 +1,17 @@
 from django import forms
-
 from models import UserProfile
 
 
 class UserProfileForm(forms.ModelForm):
+    """This class defines a model form using the 'UserProfile' model.
+    """
 
     class Meta:
+    	"""This class modifies the UserProfileForm class.
+
+        Attributes:
+            model: A model from which to construct the form.
+            field: A tuple of the form fields that are fillable.
+        """
         model = UserProfile
         fields = ('occupation', 'phonenumber', 'user_state',)
