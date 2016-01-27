ab796a2a750e9426c659dcd89951e77f62d3c938
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2016-01-27 16:58:02 +0100

    document cart app

--- troupon/cart/urls.py
+++ troupon/cart/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from cart.views import CheckoutView
 
 urlpatterns = [
+    # points to the checkout view handler
     url(r'^checkout/$',
         CheckoutView.as_view(),
         name='checkout'),
--- troupon/cart/views.py
+++ troupon/cart/views.py
@@ -7,13 +7,29 @@ from authentication.views import LoginRequiredMixin
 class CheckoutView(LoginRequiredMixin, View):
     """
     Creates a checkout page
+
+    Attributes:
+        template_name: name of the template that renders the view
+        stripe_secret_api_key: the secret API key for our stripe account
+        stripe_publishable_api_key: the publishable API key
     """
     template_name = 'cart/checkout.html'
     stripe_secret_api_key = os.getenv('STRIPE_SECRET_API_KEY')
     stripe_publishable_api_key = os.getenv('STRIPE_PUBLISHABLE_API_KEY')
 
     def get(self, request, *args, **kwargs):
-        """create a dummy checkout page"""
+        """
+        creates a dummy checkout page
+
+        Sets up a dummy page that users can pay for goods worth $23
+
+        Args:
+            request: the request object calling the view
+
+        Return:
+            a page that displays a stripe button for making payment
+            of $23
+        """
         amount = 23
         amount_in_cents = amount * 100
         payment_details = {
