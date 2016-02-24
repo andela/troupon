d85ab076c5e80aa63caf5f49c61058b216a32b2d
andela-ggikera <githinji.gikera@andela.com>
2016-02-24 12:18:50 +0300

    [finishes #114382325] Integrate cart payment functionality in the application

--- troupon/cart/views.py
+++ troupon/cart/views.py
@@ -17,38 +17,32 @@ class CheckoutView(LoginRequiredMixin, View):
     stripe_secret_api_key = os.getenv('STRIPE_SECRET_API_KEY')
     stripe_publishable_api_key = os.getenv('STRIPE_PUBLISHABLE_API_KEY')
 
-    def get(self, request, *args, **kwargs):
+    def post(self, request, **kwargs):
+        """Update imformation.
         """
-        creates a dummy checkout page
-
-        Sets up a dummy page that users can pay for goods worth $23
-
-        Args:
-            request: the request object calling the view
-
-        Return:
-            a page that displays a stripe button for making payment
-            of $23
-        """
-        amount = 23
+        amount = request.POST.get('price', 23)
+        title = request.POST.get('title')
+        description = request.POST.get('description') or "No description"
         amount_in_cents = amount * 100
+
+        # store payment details in session
         payment_details = {
+            "title": title,
             "key": self.stripe_publishable_api_key,
-            "description": "Hairless Armpits",
+            "amount": amount_in_cents,
+            "description": description,
+            "currency": "usd",
         }
 
         context = {
             "amount": amount,
+            "title": title,
+            "description": description,
             "amount_in_cents": amount_in_cents,
             "payment_details": payment_details,
         }
 
-        # store payment details in session
-        payment_details = {
-            "amount": amount_in_cents,
-            "description": "Hairless Armpits",
-            "currency": "usd",
-        }
         request.session['payment_details'] = payment_details
 
         return render(request, self.template_name, context)
+
