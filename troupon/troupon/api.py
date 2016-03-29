e8efab05a38d59222a5ca3400cce00918e528fc6
andela-ggikera <githinji.gikera@andela.com>
2016-03-29 15:07:23 +0300

    [Chore #113821767] Add url definition format to troupon/api.py

--- troupon/troupon/api.py
+++ troupon/troupon/api.py
@@ -1,2 +1,14 @@
-"""Base url configuration for DRF."""
+"""Base url configuration for DRF.
+
+The `urlpatterns` list routes URLs to the API views.
+Example:
+Using Class-based views
+    1. Add an import:  from some_app.api import HomeAPIView
+    2. Add a URL to urlpatterns:
+        url(r'^api/home/$', HomeApiView.as_view(), name='home_api')
+"""
 from django.conf.urls import url
+
+urlpatterns = [
+    # URL definition for API goes here.
+]
