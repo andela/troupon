4cc5ad71d1c99a350ef052cc383b8ee12abfc10b
Amos Omondi <amos.omondi@andela.com>
2016-04-12 10:10:02 +0300

    [Feature #113821743] Create endpoints for merchant CRUD actions on deals

--- troupon/troupon/api.py
+++ troupon/troupon/api.py
@@ -7,8 +7,15 @@ Using Class-based views
     2. Add a URL to urlpatterns:
         url(r'^api/home/$', HomeApiView.as_view(), name='home_api')
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
+from merchant.api import DealListAPIView, DealActionsAPIView
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     # URL definition for API goes here.
+    url(r'^deals/$', DealListAPIView.as_view()),
+    url(r'^deals/(?P<pk>[0-9]+)$', DealActionsAPIView.as_view()),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
