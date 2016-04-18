31af5837a0c015e27f357de76a53de6e2048334d
Jack Mwangi <jack.wachira@andela.com>
2016-04-18 14:12:13 +0300

    [Chore #115594799] Removed comments in merchant testviews

--- troupon/merchant/tests/test_views.py
+++ troupon/merchant/tests/test_views.py
@@ -1,32 +1,32 @@
-# from django.core.urlresolvers import reverse
-# from django.contrib.messages.storage.fallback import FallbackStorage
-# from django.utils.importlib import import_module
-# from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.contrib.messages.storage.fallback import FallbackStorage
+from django.utils.importlib import import_module
+from django.conf import settings
 
-# from account.tests.test_routes import UserProfileMerchantTestCase
-# from deals.tests.test_routes import set_advertiser_and_category
-# from merchant.views import CreateDealView
+from account.tests.test_routes import UserProfileMerchantTestCase
+from deals.tests.test_routes import set_advertiser_and_category
+from merchant.views import CreateDealView
 
 
-# class ApprovedMerchantTestCase(UserProfileMerchantTestCase):
+class ApprovedMerchantTestCase(UserProfileMerchantTestCase):
 
-#     """Testcase for deal creation form and deal creation. """
+    """Testcase for deal creation form and deal creation. """
 
-#     def test_merchant_can_view_deal_form(self):
+    def test_merchant_can_view_deal_form(self):
 
-#         response = self.client.get(reverse('merchant_create_deal'))
-#         self.assertEquals(response.status_code, 302)
+        response = self.client.get(reverse('merchant_create_deal'))
+        self.assertEquals(response.status_code, 302)
 
-#     def test_merchant_can_create_deal(self):
+    def test_merchant_can_create_deal(self):
 
-#         deal = set_advertiser_and_category()
-#         request = self.factory.post(
-#             reverse('merchant_create_deal'), deal)
-#         request.user = self.user
-#         engine = import_module(settings.SESSION_ENGINE)
-#         session_key = None
-#         request.session = engine.SessionStore(session_key)
-#         messages = FallbackStorage(request)
-#         setattr(request, '_messages', messages)
-#         response = CreateDealView.as_view()(request)
-#         self.assertEquals(response.status_code, 302)
+        deal = set_advertiser_and_category()
+        request = self.factory.post(
+            reverse('merchant_create_deal'), deal)
+        request.user = self.user
+        engine = import_module(settings.SESSION_ENGINE)
+        session_key = None
+        request.session = engine.SessionStore(session_key)
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+        response = CreateDealView.as_view()(request)
+        self.assertEquals(response.status_code, 302)
