580ba8cc5faa5f753d93c6e9895ca967bc47e7e2
Penina Wanjiru <penina.wanjiru@andela.com>
2016-04-25 14:33:55 +0300

    [Feature #115166813] static root

--- troupon/troupon/settings/base.py
+++ troupon/troupon/settings/base.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 LOGIN_REDIRECT_URL = '/'
@@ -144,7 +144,7 @@ LOGIN_URL = '/login/'
 LOGOUT_URL = '/'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
