93df4ed46deb5536108aedf888e07ed0f26deb6d
Penina Wanjiru <penina.wanjiru@andela.com>
2016-04-26 10:51:30 +0300

    [Feature #115166813] revert back to gitignore

--- troupon/troupon/settings/production.py
+++ troupon/troupon/settings/production.py
@@ -22,7 +22,7 @@ BOWER_PATH = '/app/node_modules/bower'
 
 
 # Enable Connection Pooling
-# DATABASES['default']['ENGINE'] = 'django_postgrespool'
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
