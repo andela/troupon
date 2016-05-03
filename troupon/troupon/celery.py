421ae3ddb6d225db40c93573a026aca5d3f7bea3
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-03 12:38:32 +0300

    [Fix] Refactor code

--- troupon/troupon/celery.py
+++ troupon/troupon/celery.py
@@ -7,9 +7,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'troupon.settings')
 
-
 app = Celery('troupon')
-
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
