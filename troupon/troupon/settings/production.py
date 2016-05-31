0a4fc7176a4f3cbc43d7e57a4aa030ed59f30bde
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-31 15:37:44 +0300

    [Fix] remove postgrespool

--- troupon/troupon/settings/production.py
+++ troupon/troupon/settings/production.py
@@ -20,7 +20,7 @@ BOWER_COMPONENTS_ROOT = os.path.join(
 BOWER_PATH = '/app/node_modules/bower'
 
 # Enable Connection Pooling
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
+# DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
