865893d5fbcf60a94673204fbf5efd90659aec70
Chukwuerika Dike <chukwuerika.dike@andela.com>
2016-06-20 16:32:38 +0100

    [Fix #119350035] Fix HoundCI identified issues

--- troupon/static/js/app.js
+++ troupon/static/js/app.js
@@ -180,8 +180,8 @@ $(document).ready(function() {
 
     var $dateFilterSelect = $('.section-heading .date-filter-select');
     $dateFilterSelect.change(function(event){
-        var $queryString = '?dtf=' + event.currentTarget.selectedIndex
-                            + '&pg=1';
+        var $queryString = '?dtf=' + event.currentTarget.selectedIndex +
+            '&pg=1';
         var $path = $(location).attr('pathname');
         $.ajax({url: $path + $queryString, success: 
             function(result) {
