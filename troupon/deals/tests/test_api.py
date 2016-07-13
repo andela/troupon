b0f1c0dee62228317e6f51c220bc78173813bc6d
Jack Mwangi <JackMwangi@Jack.local>
2016-07-13 11:05:19 +0300

    [Chore #115582173] Test feature

--- troupon/deals/tests/test_api.py
+++ troupon/deals/tests/test_api.py
@@ -1,5 +1,112 @@
+import requests
+import unittest
+
+from django.core.urlresolvers import reverse
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.test import LiveServerTestCase
+from django.test import RequestFactory, TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from django.contrib.auth.models import User
-from ..models import Deal
+from account.models import UserProfile
+from deals.models import Advertiser, Category, Deal
+from merchant.models import Merchant
+from ..models import Category, Deal, Advertiser, ALL_LOCATIONS
+
+
+TEST_USER_EMAIL = 'testuser@myemail.com'
+TEST_USER_PASSWORD = 'testpassword'
+TEST_SEARCH_TERM = "Holiday"
+TEST_SEARCH_LOCATION = "Nairobi"
+
+class CreateDeal(object):
+    def create_user(self):
+        """Create the test user"""
+        User.objects.create_user(username='mytestuser',
+                                 email=TEST_USER_EMAIL,
+                                 password=TEST_USER_PASSWORD)
+
+    def create_user_profile(self):
+        """Create the test user profile"""
+        user_object = User.objects.all()[:1].get()
+        user_profile = UserProfile.objects.create(
+            user=user_object,
+            occupation='Travel Agent',
+            intlnumber='0705123456')
+        return user_profile
+
+    def create_merchant(self):
+        """Create the test merchant"""
+        merchant = Merchant.objects.create(
+            userprofile=self.create_user_profile(),
+            intlnumber='0705123456', enabled=True,
+            approved=True, trusted=True)
+        return merchant
+    
+    def get_location(self):
+        """Returns location of user"""        
+        server_key = self.client.get(
+            "/api/serverkey/")
+        response = requests.post("https://www.googleapis.com/geolocation/v1/geolocate?key=%s" % server_key.data)
+        coordinates = response.json().get("location","")
+        lat = coordinates.get("lat","")
+        lng = coordinates.get("lng","")
+        response = requests.post("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s&key=%s" %(str(lat),str(lng),server_key.data))
+        city = response.json().get("results","")[0].get("formatted_address").split(",")[1].strip()        
+        location_index = [item[0]
+                              for item in ALL_LOCATIONS if item[1] == city]
+        return location_index
+
+    def create_deal(self):
+        """Create the test deal"""
+        merchant = self.create_merchant()
+        price = 5000
+        original_price = 6000
+        currency = 1
+        country = 2
+        location = self.get_location()[0]
+        quorum = 0
+        disclaimer = ''
+        description = 'Holiday for two to the luxurious Masai Mara.'
+        title = 'Masai Mara Holiday'
+        title2 = 'Beach holiday'
+        address = 'Masai Mara'
+        max_quantity_available = 20
+        active = True
+        advertiser_id = merchant.advertiser_ptr.id
+        date_end = "2020-09-09"
+
+        category = Category.objects.create(name="Travel N Hotels",
+                                           slug="masai-mara-holiday")
+        advertiser = Advertiser.objects.get(id=advertiser_id)
+
+        deal = Deal(
+            price=price, original_price=original_price, currency=currency,
+            country=country, location=location, category=category,
+            quorum=quorum, disclaimer=disclaimer, description=description,
+            address=address, max_quantity_available=max_quantity_available,
+            date_end=date_end, active=active, title=title,
+            advertiser=advertiser, duration=20
+        )
+
+        deal.save()
+
+
+class ServerAPITestCase(APITestCase, CreateDeal):
+
+    def setUp(self):    
+        self.create_user()
+        self.create_deal()
+
+    def test_can_get_server_key(self):
+        """Tests that server key can be obtained from environment"""
+        response = self.client.get(
+            "/api/serverkey/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_filter_deals(self):
+        """Tests that deals can be filtered according to location"""        
+        url = reverse('homepage')
+        response = self.client.get(url)
+        self.assertContains(response, 'Masai Mara Holiday')
