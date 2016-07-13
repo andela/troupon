38c6b48879b4e59da79b8fe5d9a87acd624d7009
Jack Mwangi <JackMwangi@Jack.local>
2016-07-13 11:55:01 +0300

    [Chore #115582173] Add print statements

--- troupon/deals/tests/test_api.py
+++ troupon/deals/tests/test_api.py
@@ -53,9 +53,13 @@ class CreateDeal(object):
         lat = coordinates.get("lat","")
         lng = coordinates.get("lng","")
         response = requests.post("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s&key=%s" %(str(lat),str(lng),server_key.data))
-        city = response.json().get("results","")[0].get("formatted_address").split(",")[1].strip()        
+
+        city = response.json().get("results","")[0].get("formatted_address").split(",")[1].strip()
+        print response.json().get("results","")[0]
+                 
         location_index = [item[0]
                               for item in ALL_LOCATIONS if item[1] == city]
+        print location_index
         return location_index
 
     def create_deal(self):
