41b759016bfdd4c41399d5ff3a2b417e02a91c4a
Jack Mwangi <jack.wachira@andela.com>
2016-08-01 17:09:49 +0300

    [Bug #115582173] Remove toastr

--- troupon/static/js/location.js
+++ troupon/static/js/location.js
@@ -2,7 +2,7 @@
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
       } else {
-        toastr.error("Geolocation is not supported by this browser.");
+        // toastr.error("Geolocation is not supported by this browser.");
       }
 
       function showError(error) {
@@ -21,7 +21,7 @@
             errorMsg = "An unknown error occurred.";
             break;
         }
-        toastr.error(errorMsg);
+        // toastr.error(errorMsg);
         deleteCookie();
       }
 
