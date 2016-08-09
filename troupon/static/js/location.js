bb287f41c628de3c78bc2f9b7f936ab4188234ae
Jack Mwangi <jack.wachira@andela.com>
2016-08-09 11:37:19 +0300

    [Chore #115582173] Remove toast

--- troupon/static/js/location.js
+++ troupon/static/js/location.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
             errorMsg = "An unknown error occurred.";
             break;
         }
-        toastr.error(errorMsg);
+        // toastr.error(errorMsg);
         deleteCookie();
       }
 
